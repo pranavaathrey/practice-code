4b8b381e7f4b1536235ef67f801d272c8fdb34f4
Pranav Aathrey <pranavaathrey@gmail.com>
2025-09-28 20:36:12 +0530

    doubly linked list

--- data-structures/linked-list/doubly-linkedList.c
+++ data-structures/linked-list/doubly-linkedList.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// singly linked list node
+// doubly linked list node
 typedef struct Node {
     int data;
+    struct Node *prev;
     struct Node *next;
 } Node;
  
@@ -11,6 +12,7 @@ typedef struct Node {
 Node* createNode(int data) {
     Node* newNode = (Node *)malloc(sizeof(Node));
     newNode->data = data;
+    newNode->prev = NULL;
     newNode->next = NULL;
     return newNode;
 }
@@ -30,6 +32,15 @@ void printList(Node* head) {
 
 //----------------||INSERTION OPERATIONS||----------------//
 
+// inserts a new node at the start of the list
+void insertStart(int data, Node** head) {
+    Node* newNode = createNode(data);
+    
+    newNode->next = *head;
+    if(*head != NULL)
+        (*head)->prev = newNode;
+    *head = newNode;
+}
 // inserts a new node at the end of the list
 void insertEnd(int data, Node** head) {
     Node* newNode = createNode(data);
@@ -43,19 +54,10 @@ void insertEnd(int data, Node** head) {
     while(temp->next != NULL) 
         temp = temp->next;
     temp->next = newNode;
+    newNode->prev = temp;
 }
-// inserts a new node at the start of the list
-void insertStart(int data, Node** head) {
-    Node* newNode = createNode(data);
-    
-    newNode->next = *head;
-    *head = newNode;
-}
-
 // inserts a new node at index (0-based)
-void insertAtIndex(int data, int index, Node** head) {
-    Node* newNode = createNode(data);
-    
+void insertAtIndex(int data, int index, Node** head) {    
     if(*head == NULL || index <= 0) {
         insertStart(data, head);
         return;
@@ -65,13 +67,27 @@ void insertAtIndex(int data, int index, Node** head) {
         temp = temp->next;
         index--;
     }
-    Node* nextNode = temp->next;
+    Node* newNode = createNode(data);
+    newNode->next = temp->next;
+    if(temp->next != NULL) 
+        temp->next->prev = newNode;
     temp->next = newNode;
-    newNode->next = nextNode;
+    newNode->prev = temp;
 }
 
 //----------------||DELETION OPERATIONS||----------------//
 
+// deletes the first node in the list
+void deleteStart(Node** head) {
+    if(*head == NULL) return;
+
+    Node* temp = *head;
+    *head = (*head)->next;
+    // when there's more than 1 node in the list
+    if (*head != NULL) 
+        (*head)->prev = NULL;
+    free(temp);
+}
 // deletes the last node in the list
 void deleteEnd(Node** head) {    
     if(*head == NULL) return;
@@ -86,15 +102,6 @@ void deleteEnd(Node** head) {
     free(temp->next);
     temp->next = NULL;
 }
-// deletes the first node in the list
-void deleteStart(Node** head) {
-    if(*head == NULL) return;
-
-    Node* temp = *head;
-    *head = (*head)->next;
-    free(temp);
-}
-
 // deletes the node at index (0-based)
 void deleteAtIndex(int index, Node** head) {   
     if(*head == NULL || index <= 0) {
@@ -109,7 +116,9 @@ void deleteAtIndex(int index, Node** head) {
     if(temp->next == NULL) return; // out of bounds
 
     Node* deletedNode = temp->next;
-    temp->next = temp->next->next;
+    temp->next = deletedNode->next;
+    if(deletedNode->next != NULL)
+        deletedNode->next->prev = temp;
     free(deletedNode);
 }
 // deletes the first occurrence of a node with given value
@@ -122,11 +131,12 @@ void deleteByValue(int value, Node** head) {
     while(temp->next != NULL 
         && temp->next->data != value) 
         temp = temp->next;
-
     if(temp->next == NULL) return; // not found
 
     Node* deletedNode = temp->next;
-    temp->next = temp->next->next;
+    temp->next = deletedNode->next;
+    if(deletedNode->next != NULL) 
+        deletedNode->next->prev = temp;
     free(deletedNode);    
 }
 // frees memory occupied by the list
@@ -139,21 +149,36 @@ void clearList(Node** head) {
 int main() {
     Node* list = NULL;
     
+    printf("Doubly Linked List Menu:\n");
+    printf("1: Insert Start\n2: Insert End\n3: Insert at Index\n");
+    printf("4: Delete Start\n5: Delete End\n6: Delete at Index\n7: Delete by Value\n");
+    printf("8: Print List\n9: Exit\n");
+    
     while(1) {
-        int choice; scanf("%d", &choice);
+        int choice; 
+        printf("> Enter choice: ");
+        if (scanf("%d", &choice) != 1) { 
+            printf("Invalid input. Please enter a number.\n");
+            // clear input buffer
+            while(getchar() != '\n'); 
+            continue;
+        }
         
         switch(choice) {
             case 1: { // insert at start
-                int data; scanf("%d", &data);
+                int data; printf("Enter data: ");
+                scanf("%d", &data);
                 insertStart(data, &list);
                 break;
             } case 2: { // insert at end
-                int data; scanf("%d", &data);
+                int data; printf("Enter data: ");
+                scanf("%d", &data);
                 insertEnd(data, &list);
                 break;
             } case 3: { // insert at index
-                int data, index; 
+                int data, index; printf("Enter index and data: ");
                 scanf("%d %d", &index, &data); 
+                // if beyond bounds, it just inserts at nearest position.
                 insertAtIndex(data, index, &list);
                 break;
             } case 4: // delete at start
@@ -163,11 +188,13 @@ int main() {
                 deleteEnd(&list);
                 break;
             case 6: { // delete at index
-                int index; scanf("%d", &index);
+                int index; printf("Enter index: ");
+                scanf("%d", &index);
                 deleteAtIndex(index, &list);
                 break;
             } case 7: { // delete by value
-                int value; scanf("%d", &value);
+                int value; printf("Enter value: ");
+                scanf("%d", &value);
                 deleteByValue(value, &list);
                 break;
             } case 8: // print list
@@ -178,7 +205,7 @@ int main() {
                 printf("Exiting\n");
                 return 0;
             default:
-                printf("Wrong choice\n");
+                printf("Incorrect choice\n");
                 break;
         }
     }
