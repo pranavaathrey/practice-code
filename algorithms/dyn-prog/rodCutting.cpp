159d4d072638f7bce293819dceb0340cf83379c4
Pranav Aathrey <pranavaathrey@gmail.com>
2026-02-19 22:09:19 +0530

    rod cutting

--- algorithms/dyn-prog/rodCutting.cpp
+++ algorithms/dyn-prog/rodCutting.cpp
@@ -3,21 +3,60 @@
 
 using namespace std;
 
-// O(2^n) time complexity! 
-int maximizeRevenue(int len, vector<int> &price4len) {
+struct rodCutInfo {
+    vector<int> memo;
+    vector<int> pos;
+};
+// O(n^2) time complexity
+rodCutInfo maximizeRevenue(vector<int> &price4len) {
+    int len = price4len.size();
+    vector<int> memo(len + 1, 0);
+    vector<int> pos(len + 1, 0);
+
+    for(int i = 1; i <= len; i++) {
+        int maxRev = INT_MIN;
+
+        for(int j = 1; j <= i; j++) {
+            int rev = price4len[j - 1] + memo[i - j];
+            if(rev > maxRev) {
+                maxRev = rev;
+                pos[i] = j;
+            }
+        }
+        memo[i] = maxRev; 
+    }
+    return {memo, pos};
+}
+void printCutting(rodCutInfo &info) {
+    int n = info.memo.size() - 1;
+    cout << "(Length " << n << ")\n";
+    while(n > 0) {
+        cout << info.pos[n] << "|";
+        n = n - info.pos[n];
+    }
+    cout << endl;
+}
+// O(2^n) time complexity! for len > ~40 it will take forever
+int maximizeRevenue2(int len, vector<int> &price4len) {
     if(len <= 0) return 0;
 
     int maxRevenue = 0;
     for(int i = 1; i <= len; i++) {
         int rev = price4len[i - 1] 
-            + maximizeRevenue(len - i, price4len);
+            + maximizeRevenue2(len - i, price4len);
         if(rev > maxRevenue) maxRevenue = rev;
     }
     return maxRevenue;
 }
 
 int main() {
-    vector<int> price4len = {27, 25, 29, 72, 11, 33, 70, 21, 30, 68, 47, 94, 25, 79, 45, 57, 26, 74, 13, 60, 27, 21, 85, 19, 80, 98, 51, 66, 55, 2};
-    cout << maximizeRevenue(30, price4len);
+    vector<int> price4len = {
+        1, 5, 8, 10, 13, 17, 18, 20
+    };
+    rodCutInfo info = maximizeRevenue(price4len);
+    cout << "Maximum revenue from optimal cut: \n" 
+         << info.memo[price4len.size()] << endl;
+    cout << "Optimal cutting for rod "; printCutting(info);
+    // cout << maximizeRevenue2(price4len.size(), price4len);
     return 0;
 }
